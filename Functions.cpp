ccc47a48a47d0b00173a6a618bd5270508abd623
Francisco Castillo <juanfracocastillo@hotmail.com>
2024-03-07 02:19:03 -0800

    - Finished module 07 functions cpp

--- Functions.cpp
+++ Functions.cpp
@@ -27,6 +27,8 @@ int FibonacciSequence(int fibIndex = 1);
 // but used for the same thing
 double Area(double radius);
 double Area(double radius, double height);
+double Volume(double radius);
+double Volume(double radius, double height);
 
 void DisplayArray(int array[], int length)
 {
@@ -46,6 +48,16 @@ void DisplayArray(char string[], int length)
 
 // Pass by reference
 void AreaByReference(double radius, double& result);
+void ProcessArray(double array[]);
+
+// Inline function
+inline long DoubleNumber(int inputNumber);
+
+// Automatic return type deduction
+auto AutomaticArea(double radius)
+{
+    return pi * radius * radius;
+}
 
 int main()
 {
@@ -198,8 +210,91 @@ int main()
     AreaByReference(radius, areaResult);
     cout << "The area of the circle is: " << areaResult << endl;
 
+    //------------------------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
+
+    /*
+     * So, how does this work?
+     * - Function calls cause the microprocessor to "jump" to execute the next instruction contained inside a function at a nonsequential memory location
+     * - Once all the instructions or a return statement is encountered in the function's block, the microprocessor jumps back to where it left off and
+     *   continues to execute the instructions following a function call
+     *
+     * Let's go even deeper!
+     * - The compiler converts function calls into CALL instructions for the microprocessor. CALL instructions are paired with an address in memory that points
+     *   to the next set of instructions, which are contained inside a function (function statements are contiguous memory blocks)
+     * - When the microprocessor sees the CALL instruction, it saves on the stack the memory of the next instruction to be executed after a function call and
+     *   proceeds to jump to the adress associated with the CALL instruction
+     * - Once at the new address, the microprocessor will execute all the instructions of the function and will only stop once the instruction RET is found
+     * - RET instruction indicates to the microprocessor to pop the top element of the stack, which will be the address of the next instruction after the function
+     *   call, and jump back to that address. Once back to the original address, the microprocessor continues executing the following instructions an so on...
+     *
+     * - This entire process happens at incredible speeds under the hood and usually we don't notice it.
+     * - Because of that, we are able to separate and systematically design our program to carry out different functions based on our needs
+     *
+     * - However, at times, some functions are so simple that the entire process of creating a CALL, have the microprocessor jump to another address, etc.
+     *   is too expensive for what the function actually does. For these cases, we can use the inline keyword and inline functions directly to our code.
+     * - This will tell the compiler to add the individual instructions of a function direclty at where the calling statement is, avoiding the overhead and CALL instruction
+     * - This allows for less overhead and more efficient memory managing when working with functions
+     */
+
+    cout << "\n\nInline function implementation!" << endl;
+    cout << "Enter an integer: " << endl;
+    int inputNumber{INT_MIN};
+    cin >> inputNumber;
+
+    // Calling inline
+    cout << "Double is: " << DoubleNumber(inputNumber) << endl;
+
+    /*
+     * By making the function inline we have this pseudo-calls in the microprocessor
+     * cout << message;
+     * cin >> Get user input;
+     * cout >> Double is;
+     * inputNumber << 1;
+     * cout << inputNumber;
+     *
+     * Without the inline, we would have this:
+     * cout << message;
+     * cin >> Get user input;
+     * cout >> Double is;
+     * CALL 0X0000.......
+     * STACK the address of the next cout << inputNumber
+     * (Processor proceeds to jump to another location in memory)
+     * inputNumber << 1;
+     * RET - Processor needs to return
+     * POP the stored memory address of the following cout statement -> Then jump back to initial address
+     * cout << inputNumber;
+     *
+     * USE INLINE FOR FUNCTIONS THAT PERFORM SIMPLE TASKS AND DO NOT REQUIRE A LONG TIME TO PROCESS
+     * If used on functions that require more processing power/time you can cause code bloat
+     */
+
+    // We can use the auto keyword to let the compiler figure out the return type of functions
+    // NOTE: Auto functions must be defined before they are invoked! This is because the compiler needs to know a function's return
+    // type at the point where it is used
+    // All return statements must return the same type!
+    // All recursive calls must have a return statement! (You must have a way out to deduce the type)
+
+    cout << "\n\nAutomatic return type deduction implementation!" << endl;
+    auto result = AutomaticArea(radius);
+    cout << "Return value is: " << result << " of type: " << typeid(result).name() << endl;
+
+    // QUICK NOTES ON OPTIMIZATION
+    {
+        /*
+         * Nowadays, most compilers help optimize code either in speed or size
+         *
+         * When optimizing for size, the compiler might reject some inline functions as they would bloat code and do not help performance
+         * This is helpful when creating software for devices where memory is a luxury
+         *
+         * When optimizing for speed, the compiler might do the reverse and inline blocks of code where it would make sense to, speeding up
+         * runtime processes
+         */
+    }
+
     return 0;
 }
+
 // Function definitions
 double CalculateArea(double radius)
 {
@@ -251,8 +346,31 @@ double Area(double radius, double height)
     return 2 * Area(radius) + 2 * pi * radius * height;
 }
 
+double Volume(double radius)
+{
+    return (4 * pi * radius * radius * radius) / 3;
+}
+
+double Volume(double radius, double height)
+{
+    return pi * radius * radius * height;
+}
+
 void AreaByReference(double radius, double &result)
 {
     // Notice how we are not returning a value yet we are modifying the original value of the memory location assigned to result
     result = pi * radius * radius;
+}
+
+void ProcessArray(double array[])
+{
+    for (auto element: array)
+    {
+        cout << element << endl;
+    }
+}
+
+long DoubleNumber(int inputNumber)
+{
+    return inputNumber << 1;
 }
