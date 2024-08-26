a90487126f1411e99e23110f913c2ad6bc060231
Francisco Castillo <78894868+frank-castillo@users.noreply.github.com>
2024-08-26 03:15:53 -0700

    - Expand on virtual methods - Introduce V-Table

--- OOP_Concepts/Polymorphism.cpp
+++ OOP_Concepts/Polymorphism.cpp
@@ -10,6 +10,16 @@ namespace BasicFish
     class BasicFish
     {
     public:
+        BasicFish()
+        {
+            std::cout << "BasicFish was constructed" << std::endl;
+        }
+
+        ~BasicFish()
+        {
+            std::cout << "BasicFish was destructed" << std::endl;
+        }
+
         void Swim() const
         {
             cout << "BasicFish Swims!" << endl;
@@ -19,6 +29,16 @@ namespace BasicFish
     class BasicTuna : public BasicFish
     {
     public:
+        BasicTuna()
+        {
+            std::cout << "BasicTuna was constructed" << std::endl;
+        }
+
+        ~BasicTuna()
+        {
+            std::cout << "BasicTuna was destructed" << std::endl;
+        }
+
         void Swim() const
         {
             cout << "BasicTuna Swims!" << endl;
@@ -29,6 +49,82 @@ namespace BasicFish
     {
         // Calling fish swim
         fish.Swim();
+    }
+
+    void DeleteFish(const BasicFish* fish)
+    {
+        delete fish;
+    }
+}
+
+namespace VirtualFish
+{
+    class VirtualFish
+    {
+    public:
+        VirtualFish()
+        {
+            std::cout << "VirtualFish was constructed" << std::endl;
+        }
+
+        virtual ~VirtualFish()
+        {
+            std::cout << "VirtualFish was destructed via virtual destructor" << std::endl;
+        }
+
+        virtual void Swim() const
+        {
+            cout << "VirtualFish Swims!" << endl;
+        }
+    };
+
+    class VirtualTuna : public VirtualFish
+    {
+    public:
+        VirtualTuna()
+        {
+            std::cout << "VirtualTuna was constructed" << std::endl;
+        }
+
+        ~VirtualTuna()
+        {
+            std::cout << "VirtualTuna was destructed via virtual destructor" << std::endl;
+        }
+
+        void Swim() const
+        {
+            cout << "VirtualTuna Swims!" << endl;
+        }
+    };
+
+    class VirtualCarp : public VirtualFish
+    {
+    public:
+        VirtualCarp()
+        {
+            std::cout << "VirtualCarp was constructed" << std::endl;
+        }
+
+        ~VirtualCarp()
+        {
+            std::cout << "VirtualCarp was destructed via virtual destructor" << std::endl;
+        }
+
+        void Swim() const
+        {
+            cout << "VirtualCarp Swims!" << endl;
+        }
+    };
+
+    void MakeVirtualFishSwim(const VirtualFish& fish)
+    {
+        // Calling fish swim
+        fish.Swim();
+    }
+
+    void DeleteVirtualFish(const VirtualFish* fish)
+    {
+        delete fish;
     }
 }
 
@@ -115,13 +211,13 @@ int main()
         cout << "Why do we need Polymorphism?" << endl;
         // Take a look at the following code and then the class implementation in the BasicFish namespace
 
-        constexpr BasicFish::BasicTuna tuna;
+        BasicFish::BasicTuna tuna;
 
         cout << "Using tuna.Swim() outputs: ";
         tuna.Swim(); // Here we call Tuna and its method directly
 
         cout  << "Sending tuna to MakeFishSwim(const BasicFish& fish) outputs: ";
-        MakeFishSwim(tuna); // Sending tuna as a fish avoiding slicing!
+        MakeFishSwim(tuna); // Sending tuna as a fish avoiding slicing and overriding parent method. But wait, huh?!
 
         cout << "Tuna overrides the Swim method, so why am I seeing the Fish implementation?!" << endl;
         cout << "I should be seeing: BasicTuna Swims!" << endl;
@@ -139,6 +235,160 @@ int main()
          *   the right behaviour of any class that inherits from the base class, regardless of its subclass.
          * - tl;dr: If I call a Tuna, which IS-A fish, I expect to see the Tuna swim and not a standard fish.
          * - So how do we fix this? We fix it by using VIRTUAL METHODS.
+         */
+    }
+    cout << "\n\n" << endl;
+
+    // Polymorphism using Virtual Methods
+    {
+        /*
+         * - Virtual functions as discussed before, are needed in both Dynamic and subtype polymorphism in order to
+         *   make it happen.
+         * - The first step will be to have an object of the base type, via a pointer or a reference. This object could
+         *   have been initially instantiated as a subtype of the base class or a base object. The premise here is that
+         *   we don't really care what type of object it is, all we care about is invoking the proper method regardless
+         *   of its inheritance hierarchy type.
+         * - The syntax for declaring virtual methods is as follows:
+         *  - In the base class: virtual ReturnType MethodName(ParameterList) {}
+         *  - In the derived class: ReturnType MethodName(ParameterList) {}
+         *  - As you can see, we only need to declare the base class's methods as virtual.
+         * - By making use of the virtual keyword, the compiler ensures that any overriding variant of the requested
+         *   base class method is invoked.
+         */
+
+        cout << "Using virtual methods for polymorphism" << endl;
+
+        VirtualFish::VirtualFish fish;
+        VirtualFish::VirtualTuna tuna;
+        VirtualFish::VirtualCarp carp;
+        VirtualFish::VirtualFish* pointerFish = nullptr;
+
+        cout << "Printing addresses of each fish type:" << endl;
+        cout << "fish: " << &fish << endl;
+        cout << "tuna: " << &tuna << endl;
+        cout << "carp: " << &carp << endl;
+        cout << endl;
+
+        cout << "Invoking Swim method through a pointer of type VirtualFish. Pointer points at a default fish" << endl;
+        pointerFish = &fish;
+        cout << "Pointer's address: " << pointerFish << endl;
+        pointerFish->Swim();
+        cout << endl;
+
+        cout << "Invoking Swim method through a pointer of type VirtualFish. Pointer points at a default fish" << endl;
+        pointerFish = &tuna;
+        cout << "Pointer's address: " << pointerFish << endl;
+        pointerFish->Swim();
+        cout << endl;
+
+        cout << "Invoking Swim method through a pointer of type VirtualFish. Pointer points at a default fish" << endl;
+        pointerFish = &carp;
+        cout << "Pointer's address: " << pointerFish << endl;
+        pointerFish->Swim();
+        cout << endl;
+
+        cout << "Invoking Swim method through a method. MakeVirtualFishSwim(const VirtualFish& fish), sending a fish"
+        << endl;
+        MakeVirtualFishSwim(fish);
+        cout << endl;
+
+        cout << "Invoking Swim method through a method. MakeVirtualFishSwim(const VirtualFish& fish), sending a tuna"
+        << endl;
+        MakeVirtualFishSwim(tuna);
+        cout << endl;
+
+        cout << "Invoking Swim method through a method. MakeVirtualFishSwim(const VirtualFish& fish), sending a carp"
+        << endl;
+        MakeVirtualFishSwim(carp);
+
+        /*
+         * - As you can see, the output of this section of the code is entirely different from the BasicFish
+         *   implementation.
+         * - By making use of the virtual method, even if our methods/classes/etc. don't know the exact type of the
+         *   derived object being handled, by at least providing a base class instance and a virtual method being
+         *   overridden by the derived classes, the correct method will be invoked.
+         * - This is a really powerful tool that can allow us to expand and generalize our code to cover a wide range
+         *   of use cases.
+         * - Unfortunately, with this incredible tool, new fail-safes and code security checks have to be put in place
+         *   to ensure there are no memory leaks or bugs.
+         */
+    }
+    cout << "\n\n" << endl;
+
+    // Virtual Destructors
+    {
+        /*
+         * - With C++ allowing us to allocate memory as needed, we have to take extra care on how we handle deletion
+         *   and memory release.
+         * - Polymorphism, unsurprisingly, introduces new memory issues that have to be addressed in order to ensure
+         *   our application works as expected.
+         * - Take a look at the code below but be aware of the memory issues this can cause in your system. Run at
+         *   your own risk.
+         */
+
+        cout << "Why do we need Virtual Destructors!" << endl;
+        cout << endl;
+
+        cout << "Implementation with no virtual destructor implemented. Do you want to run this code? (y/n)" << endl;
+        char answer;
+        std::cin >> answer;
+
+        if (answer == 'y' || answer == 'Y')
+        {
+            cout << "Allocating a tuna on the free memory store" << endl;
+            BasicFish::BasicTuna* tuna = new BasicFish::BasicTuna;
+
+            cout << "Deleting tuna through method: DeleteFish(const BasicFish* fish)" << endl;
+            DeleteFish(tuna);
+
+            cout << "Instantiating tuna in the stack" << endl;
+            BasicFish::BasicTuna tuna2;
+            cout << "Automatic destruction as it goes out of scope" << endl;
+
+            cout << "We have a memory leak! The tuna on the free memory store was only cleared on its Fish slice "
+                    "but the Tuna slice is now leaked, it was not cleared" << endl;
+        }
+
+        /*
+         * - The destruction of the tuna allocated on the free store was partial. When the function invokes the delete
+         *   operator, the tuna destructor is not invoked and that slice becomes lost in the system, a memory leak.
+         * - This is a big issue that we must solve if we want to ensure our application runs efficiently and safely.
+         * - This problem occurs because, just as with methods, there is no way to let the compiler know that the
+         *   object is a derived type and not just a base type. This prompts the compiler to treat the object pointer
+         *   as a base object, slicing it and tossing away the derived object slice without any sort of memory release.
+         * - Luckily for us, there is a way to solve this issue, and just as with methods, we need to declare a VIRTUAL
+         *   DESTRUCTOR!
+         * - A VIRTUAL DESTRUCTOR, just as with methods, will ensure that the destructor of a derived class is invoked
+         *   when deleting a pointer of type Base*.
+         * - Just as with methods, in order to declare a virtual destructor we need to add the virtual keyword to the
+         *   destructor in the base class and then override the method in the derived classes.
+         */
+
+        cout << "Virtual Destructor was implemented! No more leaks!" << endl;
+
+        cout << "Allocating a tuna on the free memory store" << endl;
+        VirtualFish::VirtualTuna* tuna = new VirtualFish::VirtualTuna;
+
+        cout << "Deleting tuna through method: DeleteVirtualFish(const VirtualFish* fish)" << endl;
+        DeleteVirtualFish(tuna);
+
+        {
+            cout << "Instantiating tuna in the stack" << endl;
+            VirtualFish::VirtualTuna tuna2;
+
+            cout << "Automatic destruction as it goes out of scope" << endl;
+        }
+
+        cout << "No more leaks are present and all constructors and destructors match up in number!" << endl;
+
+        /*
+         * - As you can tell by the output, all constructors and destructors are being invoked. Regardless of the type
+         *   being passed in the function Delete, now both the derived and base destructors are invoked, ensuring memory
+         *   is properly released.
+         * - Adding virtual to these methods seems to solve a lot of problems regarding memory and polymorphic classes
+         *   but, how does it all work? How does the compiler know which method belongs to each derived class if all we
+         *   are providing is a BaseType argument?
+         * - Well, that takes us to our next topic - THE VIRTUAL FUNCTION TABLE aka V-TABLE.
          */
     }
 }
